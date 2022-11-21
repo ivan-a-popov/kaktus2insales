36941bae61befe7eba40b2f8a16248b36c4806c1
Ivan Popov <ivan-a-popov@ya.ru>
2022-11-21 18:21:11 +0300

    amended log_setup

--- log_setup.py
+++ log_setup.py
@@ -5,13 +5,13 @@ import logging
 
 # create logger
 logger = logging.getLogger('kaktus2insales')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 # create file handler which logs even debug messages (set to 'error' when ran in production)
 fh = logging.FileHandler('files/kaktus2insales.log')
-fh.setLevel(logging.DEBUG)
+fh.setLevel(logging.ERROR)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
